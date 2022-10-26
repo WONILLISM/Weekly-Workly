4c592c5c27d9ea21f75df34ae4f7d9436d80bb10
mki <mki@student.42seoul.kr>
2022-10-26 16:04:54 +0900

    🔥 Remove 주석 삭제

--- frontend/src/components/Snb.tsx
+++ frontend/src/components/Snb.tsx
@@ -1,19 +1,4 @@
 import React from "react";
-// import styled from "styled-components";
-
-// const RootStyle = styled.div`
-//   height: 100%;
-//   display: flex;
-//   align-items: center;
-//   justify-content: space-between;
-//   padding: 0px 40px;
-// `;
-
-// const WrapperStyle = styled.div`
-//   display: flex;
-//   align-items: center;
-//   gap: 16px;
-// `;
 
 const Snb = () => {
   return <div>Snb</div>;
