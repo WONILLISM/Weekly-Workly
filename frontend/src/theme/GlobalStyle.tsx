1b2e8fe992d3adba057156e1f2e9c630de5a46d9
mki <mki@student.42seoul.kr>
2022-10-26 16:05:42 +0900

    💄 Style global margin, padding 변경

--- frontend/src/theme/GlobalStyle.tsx
+++ frontend/src/theme/GlobalStyle.tsx
@@ -3,18 +3,18 @@ import { createGlobalStyle } from "styled-components";
 const GlobalStyle = createGlobalStyle`
   *, *::before, *::after {
     box-sizing: border-box;
-    margin: 0;
-    padding: 0;
   }
-
+  
   html {
     width: 100%;
     height: 100%;
   }
-
+  
   body {
     font-family: "Helvetica", "Arial", sans-serif;
     line-height: normal;
+    margin: 0;
+    padding: 0;
   }
 `;
 
